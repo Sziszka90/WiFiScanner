f2f90828bae5c02ecd34262f81b617ab165601d9
Szilard <szilard.fer@gmail.com>
2021-11-06 11:28:13 +0100

    work

--- Makefile
+++ Makefile
@@ -5,9 +5,7 @@ INC_DIR_NL = /usr/include/libnl3
 $(CC) = gcc
 
 main:
-	$(CC) $(SRC_DIR)/utils.c $(SRC_DIR)/main.c -I$(INC_DIR) -I$(INC_DIR_NL) -lnl-genl-3 -lnl-3 -o main
+	$(CC) $(SRC_DIR)/utils.c $(SRC_DIR)/main.c -I$(INC_DIR_NL) -lnl-genl-3 -lnl-3 -I$(INC_DIR) -o wifi_scanner
 
 clean:
 	rm -f core *.o main
-
-gcc utils.c main.c -I/usr/include/libnl3 -lnl-genl-3 -lnl-3 -I../include -o util
--- include/utils.h
+++ include/utils.h
@@ -4,21 +4,35 @@
 extern int keepRunning;
 
 int init_nl80211(Netlink *nl, Wifi *w);
+
 int get_wifi_status(Netlink *nl, Wifi *w);
+
 void mac_addr_n2a(char *mac_addr, unsigned char *arg);
+
 void print_ssid(unsigned char *ie, int ielen);
+
 int do_scan_trigger(Netlink* nl, Wifi* w);
 
 
+
 int finish_callback(struct nl_msg *msg, void *arg);
+
 int get_wifi_name_callback(struct nl_msg *msg, void *arg);
+
 int get_wifi_info_callback(struct nl_msg *msg, void *arg);
+
 void ctrl_c_callback();
+
 int ack_callback(struct nl_msg *msg, void *arg);
+
 int family_callback(struct nl_msg *msg, void *arg);
+
 int error_callback(struct sockaddr_nl *nla, struct nlmsgerr *err, void *arg);
+
 int no_seq_check_callback(struct nl_msg *msg, void *arg);
+
 int dump_callback(struct nl_msg* msg, void* arg);
+
 int scan_callback(struct nl_msg* msg, void* arg);
 
 #endif
